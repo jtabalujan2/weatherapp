6007a3b3c5fbf019e334b6e36fc203a1cabe381e
Julian <heyyytabalu@yahoo.com>
2019-01-01 20:09:38 -0800

    adding keys

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const keys = require('./config/keys.js')
 
 //allows express access to static files in 'public' folder
 // app.use(express.static('public'))
@@ -15,5 +16,7 @@ app.get('/', function(req,res) {
 app.listen(3000,function() {
     console.log('App running on port 3000')
 })
+
+
 
     
