3ad0e052e17af0dbdd09732b7b5573a70f93fb4d
Julian Tab <heyyytabalu@yahoo.com>
2019-01-01 19:16:38 -0800

    Adding files from laptop

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+//allows express access to static files in 'public' folder
+// app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
+//setting view engine with ejs markup
+app.set('view engine', 'ejs')
+
+
+app.get('/', function(req,res) {
+    res.render('index')
+})
+
+app.listen(3000,function() {
+    console.log('App running on port 3000')
+})
+
+    
